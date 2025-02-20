22f53ca5e28a7fa4d13cd95c4367a94d39e70893
Serhii Pustovit <sergioo040199@gmail.com>
2025-02-20 17:37:00 +0200

    Pdf convertor implementation for macos

--- src/DFCApp.cs
+++ src/DFCApp.cs
@@ -1,3 +1,5 @@
+using DFC.utils;
+
 namespace DFC;
 
 using DocumentFormat.OpenXml.Packaging; 
@@ -5,15 +7,16 @@ using DocumentFormat.OpenXml.Wordprocessing;
 
 public class DFCApp
 {
-    private List<String> _files;
+    private List<String> _files = new List<String>();
 
     public DFCApp(Options options)
     {
-        
+        _files.Add(options.File);
     }
 
     public void LoadDocument()
     {
+        new Docx2PdfConvertor(_files.First()).Convert();
         // Load documents
         // Convert to PDF
     }
--- src/utils/Docx2PdfConvertor.cs
+++ src/utils/Docx2PdfConvertor.cs
@@ -1,6 +1,48 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 namespace DFC.utils;
 
 public class Docx2PdfConvertor
 {
-    
+    private String _filePath;
+    public Docx2PdfConvertor(String fileName)
+    {
+        _filePath = fileName;
+    }
+
+    public void Convert()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = "/usr/bin/osascript",
+                Arguments = "-l JavaScript convert.jxa " + _filePath + " " + _filePath.Replace("docx", "pdf"),
+                RedirectStandardOutput = true,
+            };
+            
+            Console.WriteLine("-l JavaScript convert.jxa " + _filePath + " " + _filePath.Replace("docx", "pdf") + " false");
+            
+            Process proc = new Process()
+            {
+                StartInfo = startInfo,
+            };
+            
+            proc.Start();
+            
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                var result = proc.StandardOutput.ReadLine()!;
+                Console.WriteLine(result);
+            }
+            proc.WaitForExit();
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+            
+            // TODO: Implement for windows
+        }
+    }
 }
