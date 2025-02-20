d3cf581ea4c72e83fd972e67852b9f4b360dc955
Serhii Pustovit <sergioo040199@gmail.com>
2025-02-20 14:13:13 +0200

    Move source to src folder

--- src/Options.cs
+++ src/Options.cs
@@ -5,10 +5,10 @@ using CommandLine;
 public class Options
 {
     [Option('f', "file", Required = false, HelpText = "Path to a file to be processed.")]
-    public bool File { get; set; }
+    public String File { get; set; }
         
     [Option('d', "directory", Required = false, HelpText = "Path to a directory to be processed.")]
-    public bool Directory { get; set; }
+    public String Directory { get; set; }
         
     [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
     public bool Verbose { get; set; }
